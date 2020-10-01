faa8306a6d06c94210ae5cf0b12d6ad1e8925385
kevinmeyvaert <kevin.meyvaert@gmail.com>
2020-10-01 08:45:46 +0200

    :lipstick: Move google font to styles

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,16 +1,9 @@
 import type { AppProps } from 'next/app';
-import Head from 'next/head';
 import '../styles/index.scss';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
-      <Head>
-        <link
-          href="https://fonts.googleapis.com/css2?family=Merriweather+Sans:ital,wght@0,400;1,700&display=swap"
-          rel="stylesheet"
-        />
-      </Head>
       <Component {...pageProps} />
     </>
   );
