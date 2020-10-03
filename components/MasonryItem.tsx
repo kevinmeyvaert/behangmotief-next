6b9338f8050b0eed9c5b7ab9b2cf89c032d06cc8
kevinmeyvaert <kevin.meyvaert@gmail.com>
2020-10-03 10:57:32 +0200

    :bug: Fix loading state

--- components/MasonryItem.tsx
+++ components/MasonryItem.tsx
@@ -23,13 +23,11 @@ const MasonryItem: FC<Props> = ({ src, artist, venue, slug }) => {
     <div className="c-masonry--item">
       <Link href="/album/[...slug]" as={`/album/${slug}`}>
         <a>
-          {!loaded && (
-            <img
-              src={`https://r.wannabes.be/S=W10,H10,PD2/${src}`}
-              aria-hidden="true"
-              style={{ width: '100%', filter: 'blur(5px)' }}
-            />
-          )}
+          <img
+            src={`https://r.wannabes.be/S=W10,H10,PD2/${src}`}
+            aria-hidden="true"
+            style={{ width: '100%', filter: 'blur(5px)', display: loaded ? 'none' : 'block' }}
+          />
           <img
             loading="lazy"
             srcSet={`https://r.wannabes.be/S=W1600,H1600,PD2/${src} 1600w, https://r.wannabes.be/S=W1200,H1200,PD2/${src} 1200w, https://r.wannabes.be/S=W800,H800,PD2/${src} 800w, https://r.wannabes.be/S=W400,H400,PD2/${src} 400w`}
@@ -38,7 +36,7 @@ const MasonryItem: FC<Props> = ({ src, artist, venue, slug }) => {
             alt={artist}
             onError={handleFallbackImage}
             onLoad={() => setLoaded(true)}
-            style={{ opacity: loaded ? 1 : 0, position: loaded ? 'relative' : 'absolute' }}
+            style={{ position: loaded ? 'relative' : 'absolute' }}
           />
         </a>
       </Link>
