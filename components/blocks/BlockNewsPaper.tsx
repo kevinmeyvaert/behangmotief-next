ea04f126c99dad8043889b2949c45b9609ff7e25
kevinmeyvaert <kevin.meyvaert@gmail.com>
2020-10-27 18:56:11 +0100

    :zap: Improve series performance

--- components/blocks/BlockNewsPaper.tsx
+++ components/blocks/BlockNewsPaper.tsx
@@ -3,63 +3,31 @@ import Image from 'next/image';
 
 const BlockNewsPaper = ({ contentBlock }) => {
   return (
-    <div
-      className={'duoImageBigPicture'}
-      style={!contentBlock.fullPageImage ? { paddingTop: '66.66%' } : {}}
-    >
-      {contentBlock.fullPageImage && (
-        <Image src={contentBlock.fullPageImage.url} alt={contentBlock.title} unsized />
-      )}
-      <div className={'bigWrap'} style={{ justifyContent: contentBlock.direction }}>
-        <div className={'halfWrap'}>
-          <div
-            className={'imageInsidePicture'}
-            style={{ width: `${contentBlock.firstImageSize}%` }}
-          >
-            <Image src={contentBlock.firstImage.url} alt={contentBlock.title} unsized />
-          </div>
-        </div>
-        {contentBlock.secondImage && (
+    <div className="outer">
+      <div className={'inner'} style={!contentBlock.fullPageImage ? { paddingTop: '66.66%' } : {}}>
+        {contentBlock.fullPageImage && (
+          <Image src={contentBlock.fullPageImage.url} alt={contentBlock.title} unsized />
+        )}
+        <div className={'bigWrap'} style={{ justifyContent: contentBlock.direction }}>
           <div className={'halfWrap'}>
             <div
               className={'imageInsidePicture'}
-              style={{ width: `${contentBlock.secondImageSize}%` }}
+              style={{ width: `${contentBlock.firstImageSize}%` }}
             >
-              <picture>
-                <source
-                  media="(min-width: 1440px)"
-                  srcSet={`${contentBlock.secondImage.url}?w=2000&h=2000&fm=webp&q=80`}
-                />
-                <source
-                  media="(min-width: 800px)"
-                  srcSet={`${contentBlock.secondImage.url}?w=1330&h=1330&fm=webp&q=80`}
-                />
-                <source
-                  media="(max-width: 800px)"
-                  srcSet={`${contentBlock.secondImage.url}?w=800&h=800&fm=webp&q=80`}
-                />
-
-                <source
-                  media="(min-width: 1440px)"
-                  srcSet={`${contentBlock.secondImage.url}?w=2000&h=2000&fm=jpg&q=90`}
-                />
-                <source
-                  media="(min-width: 800px)"
-                  srcSet={`${contentBlock.secondImage.url}?w=1330&h=1330&fm=jpg&q=90`}
-                />
-                <source
-                  media="(max-width: 800px)"
-                  srcSet={`${contentBlock.secondImage.url}?w=800&h=800&fm=jpg&q=90`}
-                />
-                <img
-                  alt={contentBlock.title}
-                  src={`${contentBlock.secondImage.url}?w=1330&h=1330`}
-                  className={'duoImageBlockSecondPhoto'}
-                />
-              </picture>
+              <Image src={contentBlock.firstImage.url} alt={contentBlock.title} unsized />
             </div>
           </div>
-        )}
+          {contentBlock.secondImage && (
+            <div className={'halfWrap'}>
+              <div
+                className={'imageInsidePicture'}
+                style={{ width: `${contentBlock.secondImageSize}%` }}
+              >
+                <Image src={contentBlock.secondImage.url} alt={contentBlock.title} unsized />
+              </div>
+            </div>
+          )}
+        </div>
       </div>
     </div>
   );
