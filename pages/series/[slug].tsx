b972139ef0392fdb4d288fbd1a33e4535e0b7cde
kevinmeyvaert <kevin.meyvaert@gmail.com>
2020-10-28 15:54:43 +0100

    :mag: Add tags to series

--- pages/series/[slug].tsx
+++ pages/series/[slug].tsx
@@ -6,6 +6,7 @@ import AlbumMobileHeader from '../../components/AlbumMobileHeader';
 import Navigation from '../../components/Navigation';
 import { NAVIGATION, SERIE } from '../../queries/contentful';
 import BlockNewsPaper from '../../components/blocks/BlockNewsPaper';
+import Head from 'next/head';
 
 const SeriesPage: FC<InferGetStaticPropsType<typeof getServerSideProps>> = ({
   serie,
@@ -27,6 +28,16 @@ const SeriesPage: FC<InferGetStaticPropsType<typeof getServerSideProps>> = ({
   if (!serie) return null;
   return (
     <>
+      <Head>
+        <title>{`${serie.title} - Behangmotief`}</title>
+        <meta name="description" content={`${serie.title}, a series of photos`} />
+        <meta property="og:title" content={`${serie.title} - Behangmotief`} />
+        <meta property="og:type" content="website" />
+        <meta property="og:description" content={`${serie.title}, a series of photos`} />
+        <meta name="twitter:title" content={`${serie.title} - Behangmotief`} />
+        <meta name="twitter:description" content={`${serie.title}, a series of photos`} />
+        <meta name="twitter:card" content="summary_large_image" />
+      </Head>
       <main className={isDark ? 'themed-main isDark' : 'themed-main isLight'}>
         <Navigation items={navigationItems} />
         <AlbumMobileHeader artist={serie.title} isDark={isDark} />
