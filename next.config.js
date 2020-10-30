b24c6e2ee079b0a3984ba7fe3db6d8b6f515db01
kevinmeyvaert <kevin.meyvaert@gmail.com>
2020-10-30 13:37:54 +0100

    :zap: Fix max image size

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@ const withSass = require('@zeit/next-sass');
 
 module.exports = withSass({
   images: {
-    deviceSizes: [320, 420, 768, 1024, 1200, 1800],
+    deviceSizes: [320, 420, 768, 1024, 1200, 1400],
     domains: ['images.ctfassets.net'],
   },
 });
