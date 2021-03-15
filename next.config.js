a38a9a1564081191c3176130053e2c75d384bf96
kevinmeyvaert <kevin.meyvaert@gmail.com>
2021-03-15 07:51:44 +0100

    🐛 Expose GTM env for runtime

--- next.config.js
+++ next.config.js
@@ -5,4 +5,7 @@ module.exports = withSass({
     deviceSizes: [320, 420, 768, 1024, 1200, 1400, 2500],
     domains: ['images.ctfassets.net'],
   },
+  env: {
+    NEXT_GTM: process.env.NEXT_GTM,
+  },
 });
