6166644f4164fe19ef2cc4651c7fa53d9074877b
kevinmeyvaert <kevin.meyvaert@gmail.com>
2022-04-25 20:36:23 +0200

    fix typo

--- pages/album/[...slug].tsx
+++ pages/album/[...slug].tsx
@@ -49,7 +49,10 @@ const AlbumPage: FC<InferGetStaticPropsType<typeof getStaticProps>> = ({ post })
           property="og:description"
           content={`Photos taken at the ${artist.name} show at ${venue.name}.`}
         />
-        <meta property="og:image" content={`${process.env.VERCEL_URL}/api/og-image/2022/04/23/ramkot-vooruit/og-image.png``} />
+        <meta
+          property="og:image"
+          content={`${process.env.VERCEL_URL}/api/og-image/2022/04/23/ramkot-vooruit/og-image.png`}
+        />
 
         <meta name="twitter:title" content={`${artist.name} | ${venue.name}`} />
         <meta
