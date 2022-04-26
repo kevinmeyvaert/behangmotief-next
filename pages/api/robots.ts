d93196224ee3e96f7b34c869b00c9fe91f62fd80
kevinmeyvaert <kevin.meyvaert@gmail.com>
2022-04-26 09:17:26 +0200

    fix robots

--- pages/api/robots.ts
+++ pages/api/robots.ts
@@ -4,5 +4,5 @@ import type { NextApiRequest, NextApiResponse } from 'next';
 
 export default (req: NextApiRequest, res: NextApiResponse) => {
   res.status(200).send(`User-agent: *
-  Disallow: /`);
+  Allow: /`);
 };
