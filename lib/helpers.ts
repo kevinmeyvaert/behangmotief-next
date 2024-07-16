cd89f736b59116407b953f0ca251eee3a5958b9d
kevinmeyvaert <kevin.meyvaert@gmail.com>
2024-07-16 19:45:17 +0200

    chore: fallback custom album cover

--- lib/helpers.ts
+++ lib/helpers.ts
@@ -2,8 +2,8 @@ import { Post } from '../types/wannabes.types';
 
 export const filterOwnAlbums = (albums?: Post[]) => {
   return albums?.map((album) => {
-    if (album.thumbnail.photographer.firstName !== 'Kevin') {
-      const kevThumbnail = album.images.filter((i) => i.photographer.firstName === 'Kevin')[0];
+    if (album.thumbnail.photographer?.firstName !== 'Kevin') {
+      const kevThumbnail = album.images.filter((i) => i.photographer?.firstName === 'Kevin')[0];
       return {
         ...album,
         thumbnail: {
